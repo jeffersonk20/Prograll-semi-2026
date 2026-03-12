75cada0e161484900af2b6442c8b1b154eb27fbe
JEFFERSON STEVEN CASTILLO LEIVA <jeffersonk20castillo@gmail.com>
2026-03-12 12:32:32 -0600

    Entorno de trabajo para la tienda de productos creado; definicion del nombre de la app como JEFF-PHONE.

--- miPrimeraApp/app/src/main/java/com/example/miprimeraapp/MainActivity.java
+++ miPrimeraApp/app/src/main/java/com/example/miprimeraapp/MainActivity.java
@@ -11,12 +11,5 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-
-    }
+//COMIENZA A PROGRAMAR AQUI.
 }
