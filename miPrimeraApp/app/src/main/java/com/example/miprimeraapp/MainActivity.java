916a0fedd3db3ec53e2be1c15f6429e8399f7d72
JEFFERSON STEVEN CASTILLO LEIVA <jeffersonk20castillo@gmail.com>
2026-04-08 19:08:13 -0600

    Terminacion de la aplicacion de amigos, puede agregar, modificar, eliminar y buscar, toma fotos y la muestra

--- miPrimeraApp/app/src/main/java/com/example/miprimeraapp/MainActivity.java
+++ miPrimeraApp/app/src/main/java/com/example/miprimeraapp/MainActivity.java
@@ -1,5 +1,4 @@
 package com.example.miprimeraapp;
-
 import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
@@ -15,6 +14,8 @@ import androidx.annotation.Nullable;
 import androidx.core.content.FileProvider;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import org.json.JSONObject;
 
 import java.io.File;
 import java.text.SimpleDateFormat;
@@ -43,8 +44,39 @@ public class MainActivity extends Activity {
         btn.setOnClickListener(v->guardarAmigo());
 
         fab = findViewById(R.id.fabListaAmigo);
+        fab.setOnClickListener(v->regresarListaAmigos());
 
+        mostrarDatosAmigos();
+    }
+    private void mostrarDatosAmigos(){
+        try{
+            Bundle parametros = getIntent().getExtras();
+            accion = parametros.getString("accion");
+            if(accion.equals("modificar")){
+                JSONObject datos = new JSONObject(parametros.getString("amigos"));
+                idAmigo = datos.getString("idAmigo");
 
+                tempVal = findViewById(R.id.txtNombreAmigos);
+                tempVal.setText(datos.getString("nombre"));
+
+                tempVal = findViewById(R.id.txtDireccionAmigos);
+                tempVal.setText(datos.getString("direccion"));
+
+                tempVal = findViewById(R.id.txtTelefonoAmigos);
+                tempVal.setText(datos.getString("telefono"));
+
+                tempVal = findViewById(R.id.txtEmailAmigos);
+                tempVal.setText(datos.getString("email"));
+
+                tempVal = findViewById(R.id.txtDuiAmigos);
+                tempVal.setText(datos.getString("dui"));
+
+                urlFoto = datos.getString("foto");
+                img.setImageURI(Uri.parse(urlFoto));
+            }
+        }catch (Exception e){
+            mostrarMsg("Error al mostrar los datos: "+ e.getMessage());
+        }
     }
     private void tomarFoto(){
         tomarFotoIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
@@ -108,8 +140,14 @@ public class MainActivity extends Activity {
         String[] datos = {idAmigo, nombre, direccion, tel, email, dui, urlFoto};
         db.administrar_amigos(accion, datos);
         mostrarMsg("Registro de amigo guardado con exito.");
+
+        regresarListaAmigos();
     }
     private void mostrarMsg(String msg){
         Toast.makeText(this, msg, Toast.LENGTH_LONG).show();
+    }
+    private void regresarListaAmigos(){
+        Intent intent = new Intent(this, lista_amigos.class);
+        startActivity(intent);
     }
 }
